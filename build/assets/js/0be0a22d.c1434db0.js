df77d2044efb8d90a0e5e9bc73a388035d4242b4
midilab <contact@midilab.co>
2025-04-29 17:20:22 -0300

    removing local build directory, deploying now via github actions workflow instead

--- build/assets/js/0be0a22d.c1434db0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktinasaurus=self.webpackChunktinasaurus||[]).push([[705],{83769:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- build/assets/js/a8d6988d.e7ee2841.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktinasaurus=self.webpackChunktinasaurus||[]).push([[96],{15745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
