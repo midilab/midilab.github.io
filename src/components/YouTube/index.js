ebcf077a48ec8edc5c12dbdb1359734e7e0fc516
midilab <contact@midilab.co>
2025-06-26 08:56:22 -0300

    added param support for Youtube component for start video positioning

--- src/components/YouTube/index.js
+++ src/components/YouTube/index.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { TinaField } from 'tinacms';
+import { TinaField } from "tinacms";
 import styles from "./index.module.css";
 
 const YouTubeEmbed = ({ videoId }) => {
+  // Parse videoId and query parameters
+  const parseVideoIdAndParams = (input) => {
+    if (!input) return { id: "", params: "" };
+
+    const [idPart, ...paramParts] = input.split("?");
+    const paramsString = paramParts.join("&"); // handle extra `?` if any
+    return {
+      id: idPart,
+      params: paramsString ? `&${paramsString}` : "",
+    };
+  };
+
+  const { id: cleanVideoId, params: extraParams } =
+    parseVideoIdAndParams(videoId);
+
+  const embedUrl = `https://www.youtube.com/embed/${cleanVideoId}?rel=0&modestbranding=0&controls=1&showinfo=1&fs=1&wmode=transparent${extraParams}`;
+
   return (
     <div className={styles.container}>
       <TinaField name="videoId">
         <iframe
           width="560"
           height="315"
-          src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=0&controls=1&showinfo=1&fs=1&wmode=transparent`}
-          data-src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=0&controls=1&showinfo=1&fs=1&wmode=transparent`}
+          src={embedUrl}
+          data-src={embedUrl}
           frameBorder="0"
-          allowfullscreen=""
-          class="tcb-responsive-video"
+          allowFullScreen
+          className="tcb-responsive-video"
           data-provider="youtube"
         />
       </TinaField>
